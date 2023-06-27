c51fcfd382acf5f2b9895ca0421d285cdd0eef2e
Nikolay Ignatovich <nikademis04@gmail.com>
2023-06-27 12:28:37 -0400

    Run Initial Migrations

--- api/migrations/001_sample_migration.py
+++ api/migrations/001_sample_migration.py
@@ -2,37 +2,19 @@ steps = [
     [
         # "Up" SQL statement
         """
-        CREATE TABLE dummy (
+        CREATE TABLE users (
             id SERIAL PRIMARY KEY NOT NULL,
-            required_limited_text VARCHAR(1000) NOT NULL,
-            required_unlimited_text TEXT NOT NULL,
-            required_date_time TIMESTAMP NOT NULL,
-            automatically_set_date_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            required_integer INTEGER NOT NULL,
-            required_money MONEY NOT NULL
+            username VARCHAR(20) NOT NULL,
+            email VARCHAR(50) NOT NULL,
+            password VARCHAR(50) NOT NULL,
+            first_name VARCHAR(100) NOT NULL,
+            last_name VARCHAR(100) NOT NULL,
+            avatar VARCHAR(256)
         );
         """,
         # "Down" SQL statement
         """
-        DROP TABLE dummy;
-        """
-    ],
-    [
-        # "Up" SQL statement
-        """
-        CREATE TABLE big_dummy (
-            id SERIAL PRIMARY KEY NOT NULL,
-            required_limited_text VARCHAR(1000) NOT NULL,
-            required_unlimited_text TEXT NOT NULL,
-            required_date_time TIMESTAMP NOT NULL,
-            automatically_set_date_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            required_integer INTEGER NOT NULL,
-            required_money MONEY NOT NULL
-        );
+        DROP TABLE users;
         """,
-        # "Down" SQL statement
-        """
-        DROP TABLE big_dummy;
-        """
     ]
 ]
