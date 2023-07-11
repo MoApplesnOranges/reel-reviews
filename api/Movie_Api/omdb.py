3f4b4603596eb798b5dc9961da187fcfa975afaa
kevalm09 <kevalm09@gmail.com>
2023-07-11 17:19:10 -0400

    Movie api search first pass

--- api/Movie_Api/omdb.py
+++ api/Movie_Api/omdb.py
@@ -5,7 +5,7 @@ import requests
 
 def search_title(string):
     querystring = {
-        "exact": "false",
+        "exact": "true",
         "titleType": "movie",
     }
     url = f"https://moviesdatabase.p.rapidapi.com/titles/search/title/{string}"
@@ -14,24 +14,19 @@ def search_title(string):
         "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
     }
     response = requests.get(url, headers=headers, params=querystring)
-    # result = json.dumps(response)
     content = response.json()
     # try:
     movie_list = []
-    for movie in range(len(content["results"])):
-        film = {
-            "image": content["results"][movie]["primaryImage"]["url"],
-            "title": content["results"][movie]["originalTitleText"]["text"],
-            "release_date": content["results"][movie]["releaseDate"],
-        }
-        movie_list.append(
-            film
-            # {
-            #     "image": movie["primaryImage"]["url"],
-            #     "title": movie["originalTitleText"]["text"],
-            #     "release_date": movie["releaseDate"],
-            # }
-        )
-    return movie_list
+    for movie in content["results"]:
+        image = movie["primaryImage"]
+        if image != None:
+            films = {
+                "image": movie["primaryImage"]["url"],
+                "title": movie["originalTitleText"]["text"],
+                "release_year": movie["releaseDate"]["year"],
+            }
+
+            movie_list.append(films)
+    return {"results": movie_list}
     # except (KeyError, IndexError):
     #     return None
