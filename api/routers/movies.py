916cdbb42dbfe8e11cfb4b5e6e68655ba8edcbb0
Kevin Almonte <kevalm09@gmail.com>
2023-07-13 20:45:55 +0000

    Movies api

--- api/routers/movies.py
+++ api/routers/movies.py
@@ -6,7 +6,7 @@ from fastapi import (
     status,
     Request,
 )
-from Movie_Api.omdb import search_title
+from Movie_Api.omdb import search_title, get_title
 from queries.movies import MovieIn
 
 router = APIRouter()
@@ -15,3 +15,11 @@ router = APIRouter()
 @router.get("/api/movies")
 async def search(search: str):
     return search_title(search)
+
+
+@router.get("/api/movie")
+async def get_title_id(id: str):
+    return get_title(id)
+
+
+# hi
